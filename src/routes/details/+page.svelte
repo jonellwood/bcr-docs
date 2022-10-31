b70b9a54da8cb3a570fd1605ca373d7aa0697a7e
Jon Ellwood <jonellwood@gmail.com>
2022-10-31 12:17:54 -0400

    content added

--- src/routes/details/+page.svelte
+++ src/routes/details/+page.svelte
@@ -5,6 +5,8 @@
     import buttonsCircles from "../../images/buttons-with-circles.png";
     import updated from "../../images/request-status-updated.png";
     import logs from "../../images/logs.png";
+    import logging from "../../images/details-showing-logging.png";
+    
   </script>
   
   <div class='main'>
@@ -12,38 +14,34 @@
     <hr>
     <h3>Viewing the Details of a Request</h3>
     <div class="list-with-image">
-      <img src={details} alt="details view" width="90%" />
-      <div>
+      
+      <p>
+        By selecting "details" from the dashbaord you can view the details of
+        each request. In this example you can see that <mark
+        ><b>"Jon Ellwood"</b></mark 
+        >
+        is requesting <mark><b>"$1000"</b></mark> from moved from
+        <mark><b>"Post it Notes" to "Coffee"</b></mark> from within the Auditor Department
+        for a very silly reason.
+      </p>
+      <img src={details} alt="details view"  />
+        </div>
+        <hr>
+        <div>
+          <p>
+            Based on your role in the system you will be presented with buttons that
+            allow you to take action on the request.
+          </p>
         <p>
-          By selecting "details" from the dashbaord you can view the details of
-          each request. In this example you can see that <mark
-            ><b>"Jon Ellwood"</b></mark
-          >
-          is requesting <mark><b>"$1000"</b></mark> from moved from
-          <mark><b>"Post it Notes" to "Coffee"</b></mark> from within the Auditor Department
-          for a very silly reason.
-        </p>
-        <p>
-          Based on your role in the system you will be presented with buttons that
-          allow you to take action on the request.
-        </p>
-        <p>
-          As you can see in this example - this user can only enter and view or
+          As you can see in the example above - this user can only enter and view or
           <a href="/printing">print</a> requests.
         </p>
-      </div>
+      
+        <!-- <img src={moreButtons} alt="more button examples" width="80%" /> -->
     </div>
-    <hr>
     <div class="list-with-image">
-      <div>
-        <p>
-          In this example we can see that Jerri Christmas is requesting $9,999 be
-          moved from "Horseshoes to Horsehats from within the Animal Center
-          becuase she needs more cowbell??
-        </p>
+      
         
-      </div>
-      <img src={moreButtons} alt="more button examples" width="80%" />
     </div>
     <hr>
     <div>
@@ -65,6 +63,7 @@
       <div>
         <p>The action will also be logged in the system logs and be reflected in the details view of the request as well.</p>
         <img src={logs} alt="just some logs. Not the cool ones from trees but entries made into a logging system to keep track of events" width="100%"/>
+        <img src={logging} alt="showing logging. Not harvesting timber - the other one" width="60%"/>
       </div>
     </div>
   </div>
@@ -74,16 +73,12 @@
       text-align: left;
     }
   
-    .list-with-image {
+    /* .list-with-image {
       display: grid;
       grid-template-columns: 1fr 1fr;
-    }
+    } */
     img{
         margin: 20px;
-    }
-    .main{
-        margin: 30px;
-        max-width: 1080px;
     }
     a{
       text-decoration: none;
@@ -91,12 +86,17 @@
       /* color: #005677; */
       color: #58c9e8;
       font-weight: 400;
-
+      
     }
     figure{
       display: grid;
       grid-template-columns: 1fr 1fr;
     }
+    .main{
+        margin: 30px;
+        max-width: 1080px;
+    }
+
     
   </style>
   
