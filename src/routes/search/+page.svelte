284a7c8ddef5992073cf9f71b719f72ff3c7e409
Jon Ellwood <jonellwood@gmail.com>
2022-11-03 14:53:11 -0400

    idk

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -4,7 +4,13 @@ import search from "../../images/user-search-dashboard.png";
 <div class='main'>
     <h1>Using the Search Feature</h1>
     <hr>
-    <img src={search} alt="serach dashboard" />
+    <img src={search} alt="search dashboard" />
+    <img src={searchUser} alt="search by user" />
+    <img src={searchUserResult} alt="result from search by user" />
+    <img src={searchAmount} alt="search by amount" />
+    <img src={searchAmountResult} alt="restult from search by amount" />
+    <img src={searchAmountResult} alt="results from search by amount" />
+
 </div>
 <style>
     img{
